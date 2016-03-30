744373c6803774b2aa8e6e87177667a97d81f079
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 12:28:45 +0800

    route middleware

--- routes/routes.php
+++ routes/routes.php
@@ -3,7 +3,7 @@
 Route::group(['prefix' => 'billplz'], function () {
     Route::any('webhook', '\Klsandbox\BillplzRoute\Http\Controllers\BillplzController@postWebhook');
 
-    Route::group(['middleware' => ['auth.admin']], function () {
+    Route::group(['middleware' => ['role:admin']], function () {
         Route::get('new-collection', 'Klsandbox\BillplzRoute\Http\Controllers\BillplzController@getNewCollection');
     });
 });
