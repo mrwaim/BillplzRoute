60ef1ae43b00a3f8d590227c9e9c22129fa6b70a
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 15:36:15 +0800

    header

--- views/new-collection.blade.php
+++ views/new-collection.blade.php
@@ -1,21 +1,7 @@
 @extends('app')
 
 @section('page-header')
-    <h2>Billplz</h2>
-
-    <div class="right-wrapper pull-right">
-        <ol class="breadcrumbs">
-            <li>
-                <a href="index.html">
-                    <i class="fa fa-home"></i>
-                </a>
-            </li>
-            <li><span>Billplz</span></li>
-            <li><span>New Collection</span></li>
-        </ol>
-
-        <div class="sidebar-right-toggle"></div>
-    </div>
+    @include('elements.page-header', ['section_title' => 'Billplz', 'page_title' => 'New Collection'])
 @endsection
 
 @section('content')
